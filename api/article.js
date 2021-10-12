e0e35f5c10f15125a968e47b55fe2a3784cbf4b7
liquid <1695829731@qq.com>
2021-10-12 19:04:03 +0800

    sec

--- api/article.js
+++ api/article.js
@@ -1,8 +1,25 @@
 import request from '@/utils/request'
 
+import Qs from 'qs'
+
 export function getArticle() {
     return request({
         url:'/findAll',
         method:'post'
+    })
+}
+
+export function getArticleById(data) {
+    return request({
+        url:'/findArticleById',
+        method:'post',
+        transformRequest: [function (data) {
+            // 对 data 进行任意转换处理
+            return Qs.stringify(data)
+        }],
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        data:data,
     })
 }
